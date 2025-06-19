ea146ed53e85251725bcab3c6bc87bb9fa35477b
Jaidson Martins <jaidson.mda@gmail.com>
2025-06-19 06:59:37 -0300

    Initial commit

---
+++ src/main/java/dev/jaidson/geestacar/GeestacarApplication.java
@@ -0,0 +1,13 @@
+package dev.jaidson.geestacar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeestacarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeestacarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/jaidson/geestacar/GeestacarApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jaidson.geestacar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeestacarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
